97b3d3e5a0e0d0de4116b8d27048e16b2cfcf2a2
Lisranda <xlisranda@gmail.com>
2018-09-21 14:06:59 -0500

    Initial Commit

---
+++ Code_YDIR/Assets/MapGridGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGridGenerator : MonoBehaviour {
+	[SerializeField] LayerMask terrainLayer;
+
+	void Start () {
+		CreateGrid ();
+		
+	}
+
+	void Update () {
+		
+	}
+
+	void CreateGrid () {
+		Vector3 startLocation = new Vector3 (0f, 25f, 0f);
+		List<Vector3> toCheckLocations = new List<Vector3> ();
+		List<Vector3> checkedLocations = new List<Vector3> ();
+		toCheckLocations.Add (startLocation);
+
+		while (toCheckLocations.Count > 0) {
+			if (!Physics.Raycast (toCheckLocations [0], Vector3.down, 50f, terrainLayer)) {
+				checkedLocations.Add (toCheckLocations [0]);
+				toCheckLocations.Remove (toCheckLocations [0]);
+				continue;
+			}
+			
+			Tile tile = new Tile (toCheckLocations [0]);
+						
+			if (!checkedLocations.Contains (toCheckLocations [0] + Vector3.forward) && !toCheckLocations.Contains (toCheckLocations [0] + Vector3.forward))
+				toCheckLocations.Add (toCheckLocations [0] + Vector3.forward);
+			if (!checkedLocations.Contains (toCheckLocations [0] + Vector3.back) && !toCheckLocations.Contains (toCheckLocations [0] + Vector3.back))
+				toCheckLocations.Add (toCheckLocations [0] + Vector3.back);
+			if (!checkedLocations.Contains (toCheckLocations [0] + Vector3.right) && !toCheckLocations.Contains (toCheckLocations [0] + Vector3.right))
+				toCheckLocations.Add (toCheckLocations [0] + Vector3.right);
+			if (!checkedLocations.Contains (toCheckLocations [0] + Vector3.left) && !toCheckLocations.Contains (toCheckLocations [0] + Vector3.left))
+				toCheckLocations.Add (toCheckLocations [0] + Vector3.left);
+			
+			checkedLocations.Add (toCheckLocations [0]);
+			toCheckLocations.Remove (toCheckLocations [0]);
+		}
+	}
+}
---
+++ Code_YDIR/Assets/Tile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+	public static List<Tile> tileList;
+
+	Vector3 coords;
+
+	public Tile (Vector3 coords){
+		tileList.Add (this);
+		this.coords = coords;
+	}
+
+	public Vector3 GetCoords () {
+		return coords;
+	}
+}
